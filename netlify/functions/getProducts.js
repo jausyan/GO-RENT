fcdf29245e5992ca02b1b130ff265a26a2101a73
jausyan-best <aptmeranti305@gmail.com>
2026-02-24 21:51:36 +0700

    add product func

---
+++ netlify/functions/getProducts.js
@@ -0,0 +1,26 @@
+const { createClient } = require("@supabase/supabase-js");
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
+
+exports.handler = async function () {
+  try {
+    const { data, error } = await supabase
+      .from("products")
+      .select("*");
+
+    if (error) throw error;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
